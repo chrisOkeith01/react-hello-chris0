edd8e6488cd582939fecb2a807bbe63d1253d8bb
chrisOkeith01 <christopherkeith70@gmail.com>
2025-01-06 20:08:41 -0500

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,10 @@
-//import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
-
-//import your own components
-import Home from "./component/home.jsx";
-
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
-
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
